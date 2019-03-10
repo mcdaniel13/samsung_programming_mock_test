fe36ad0225a795b0330b6357a835f6dcdebb4432
Moongee Cho <whansrl92@gmail.com>
2019-03-11 02:38:44 +0900

    Update dessert_cafe.cpp

--- dessert_cafe/dessert_cafe.cpp
+++ dessert_cafe/dessert_cafe.cpp
@@ -1,103 +1,120 @@
-// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <algorithm>
 using namespace std;
 
-vector<vector<int>> dessertMap;
-vector<bool> appearedDessert(101, false);
-
-int moving[4][2] = { {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
-
-struct Info {
+class Point {
+public:
+    int x;
+    int y;
+    vector<bool> visited;
+    int count;
     int startX;
     int startY;
-    int count;
-    bool isVisited = false;
+
+    Point(int curX, int curY, int stX, int stY, int cnt, vector<bool> isVisited) {
+        x = curX;
+        y = curY;
+        startX = stX;
+        startY = stY;
+        count = cnt;
+        visited = isVisited;
+    }
 };
 
-int maxCycle;
-void isCycle(vector<vector<Info>> &mapInfo, int x, int y, int prevX, int prevY) {
-
-    for (int k = 0; k < 4; k++) {
-        if (x + moving[k][0] >= 0 && x + moving[k][0] < dessertMap.size() && y + moving[k][1] >= 0 && y + moving[k][1] < dessertMap.size()) {
-            int newX = x + moving[k][0];
-            int newY = y + moving[k][1];
-            //cout << "mapInfo = " << mapInfo[x][y].startX << " , " << mapInfo[x][y].startY << endl;
-            //cout << "prev = " << prevX << " , " << prevY << endl;
-            //cout << "new = " << newX << " , " << newY << endl;
-            if (!mapInfo[newX][newY].isVisited && !appearedDessert[dessertMap[newX][newY]]) {
-                appearedDessert[dessertMap[newX][newY]] = true;
-                mapInfo[newX][newY].count = mapInfo[x][y].count + 1;
-                //cout << "newDessert Appears! - start = [" << mapInfo[x][y].startX << ", " << mapInfo[x][y].startY << "] : (" << x << ", " << y << ") -> (" << x + moving[k][0] << " ," << y + moving[k][1] << ") resCount = " << mapInfo[newX][newY].count << endl;
-                mapInfo[newX][newY].startX = mapInfo[x][y].startX;ddddddd
-                mapInfo[newX][newY].startY = mapInfo[x][y].startY;
-                mapInfo[newX][newY].isVisited = true;
-                isCycle(mapInfo, newX, newY, x, y);
-                mapInfo[newX][newY].isVisited = false;
-            }
-            else if ((prevX != mapInfo[x][y].startX && prevY != mapInfo[x][y].startY != 1) && (newX == mapInfo[x][y].startX && newY == mapInfo[x][y].startY)) {
-                //cout << "cycle Appears! - start = [" << mapInfo[x][y].startX << ", " << mapInfo[x][y].startY << "] : (" << x << ", " << y << ") -> (" << x + moving[k][0] << " ," << y + moving[k][1] << ") resCount = " << mapInfo[x][y].count << endl;
-                int currCount = mapInfo[x][y].count;
-                if (currCount > maxCycle)
-                    maxCycle = currCount;
-            }
-        }
-    }
-    appearedDessert[dessertMap[x][y]] = false;
-    mapInfo[x][y].isVisited = false;
-}
-
-
-void solve(int m) {
-    maxCycle = -1;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-            vector<vector<Info>> mapInfo(m, vector<Info>(m));
+int solve(const vector<vector<int>> &dessertMap, int m) {
+    vector<Point> a;
+    vector<Point> b;
+    vector<Point> c;
+    vector<Point> d;
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = 1; j < m - 1; j++) {
+            vector<bool> isVisited(101, false);
+            int count = 1;
+            int startX = i;
+            int startY = j;
             int x = i;
             int y = j;
-            if ((x == 0 && y == 0) || (x == 0 && y == m - 1) || (x == m - 1 && y == 0) || (x == m - 1 || y == m - 1))
-                continue;
-            mapInfo[x][y].count = 1;
-            mapInfo[x][y].startX = x;
-            mapInfo[x][y].startY = y;
-
-            appearedDessert[dessertMap[x][y]] = true;
-            mapInfo[x][y].isVisited = true;
-            isCycle(mapInfo, x, y, x, y);
-            appearedDessert[dessertMap[x][y]] = false;
-
+            isVisited[dessertMap[x][y]] = true;
+            while (x + 1 >= 0 && x + 1 < m - 1 && y + 1 >= 0 && y + 1 < m && isVisited[dessertMap[x + 1][y + 1]] == false) {
+                x = x + 1;
+                y = y + 1;
+                count++;
+                isVisited[dessertMap[x][y]] = true;
+                Point tempPoint(x, y, startX, startY, count, isVisited);
+                a.push_back(tempPoint);
+            }
         }
     }
-}
 
+    for (int i = 0; i < a.size(); i++) {
+        int x = a[i].x;
+        int y = a[i].y;
+        int count = a[i].count;
+        while (x + 1 >= 0 && x + 1 < m && y - 1 >= 1 && y - 1 < m && a[i].visited[dessertMap[x + 1][y - 1]] == false) {
+            x = x + 1;
+            y = y - 1;
+            count++;
+            a[i].visited[dessertMap[x][y]] = true;
+            Point tempPoint(x, y, a[i].startX, a[i].startY, count, a[i].visited);
+            b.push_back(tempPoint);
+        }
+    }
+
+    for (int i = 0; i < b.size(); i++) {
+        int x = b[i].x;
+        int y = b[i].y;
+        int count = b[i].count;
+        while (x - 1 >= 1 && x - 1 < m && y - 1 >= 0 && y - 1 < m && b[i].visited[dessertMap[x - 1][y - 1]] == false) {
+            x = x - 1;
+            y = y - 1;
+            count++;
+            b[i].visited[dessertMap[x][y]] = true;
+            Point tempPoint(x, y, b[i].startX, b[i].startY, count, b[i].visited);
+            c.push_back(tempPoint);
+        }
+    }
+
+    int maxCount = -1;
+    for (int i = 0; i < c.size(); i++) {
+        int x = c[i].x;
+        int y = c[i].y;
+        int count = c[i].count;
+        while (x - 1 >= 0 && x - 1 < m && y + 1 >= 0 && y + 1 < m && c[i].visited[dessertMap[x - 1][y + 1]] == false) {
+            x = x - 1;
+            y = y + 1;
+            count++;
+            c[i].visited[dessertMap[x][y]] = true;
+            Point tempPoint(x, y, c[i].startX, c[i].startY, count, c[i].visited);
+            d.push_back(tempPoint);
+        }
+        if (x - 1 == c[i].startX && y + 1 == c[i].startY) {
+            maxCount = max(maxCount, count);
+        }
+    }
+
+    return maxCount;
+}
 
 int main() {
     int n;
     cin >> n;
-    vector<int> res;
+    vector<int> res(n);
     for (int i = 0; i < n; i++) {
-        dessertMap.clear();
         int m;
         cin >> m;
-        dessertMap.resize(m, vector<int>(m));
 
+        vector<vector<int>> dessertMap(m, vector<int>(m));
         for (int j = 0; j < m; j++) {
             for (int k = 0; k < m; k++) {
                 cin >> dessertMap[j][k];
             }
         }
 
-        solve(m);
-        res.push_back(maxCycle);
-
+        res[i] = solve(dessertMap, m);
     }
 
-    for (int i = 0; i < res.size(); i++)
-        cout << res[i] << endl;
-    return 0;
+    for (int i = 0; i < n; i++) {
+        cout << "#" << i + 1 << " " << res[i] << endl;
+    }
 }
-
