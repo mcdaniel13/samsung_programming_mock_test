4a5204b3f5bc501eb93a76e24a9663c6966f8b15
Moongee Cho <whansrl92@gmail.com>
2019-03-18 01:57:42 +0900

    Update dessert_cafe.cpp

--- dessert_cafe/dessert_cafe.cpp
+++ dessert_cafe/dessert_cafe.cpp
@@ -1,105 +1,107 @@
+#include "pch.h"
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-class Point {
-public:
-    int x;
-    int y;
-    vector<bool> visited;
-    int count;
-    int startX;
-    int startY;
-
-    Point(int curX, int curY, int stX, int stY, int cnt, vector<bool> isVisited) {
-        x = curX;
-        y = curY;
-        startX = stX;
-        startY = stY;
-        count = cnt;
-        visited = isVisited;
-    }
-};
-
 int solve(const vector<vector<int>> &dessertMap, int m) {
-    vector<Point> a;
-    vector<Point> b;
-    vector<Point> c;
-    vector<Point> d;
-    for (int i = 0; i < m - 1; i++) {
-        for (int j = 1; j < m - 1; j++) {
-            vector<bool> isVisited(101, false);
-            int count = 1;
-            int startX = i;
-            int startY = j;
-            int x = i;
-            int y = j;
-            isVisited[dessertMap[x][y]] = true;
-            while (x + 1 >= 0 && x + 1 < m - 1 && y + 1 >= 0 && y + 1 < m && isVisited[dessertMap[x + 1][y + 1]] == false) {
-                x = x + 1;
-                y = y + 1;
-                count++;
-                isVisited[dessertMap[x][y]] = true;
-                Point tempPoint(x, y, startX, startY, count, isVisited);
-                a.push_back(tempPoint);
+    int result = -1;
+    for (int i = 0; i < dessertMap.size(); i++) {
+        for (int j = 0; j < dessertMap[i].size(); j++) {
+
+            for (int a = 1; a < m - 1; a++) {
+                for (int b = 1; b < m - 1; b++) {
+                    if (j - b >= 0 && i + a + b < m && j + a < m && (a + b) * 2 > result) {
+                        //cout << "result = " << result << " start = (" << i << ", " << j << ") where a = " << a << " b = " << b << endl;
+                        bool isAble = true;
+                        vector<bool> visited(101, false);
+                        visited[dessertMap[i][j]] = true;
+                        int curi = i;
+                        int curj = j;
+                        for (int n = 0; n < a; n++) {
+                            curi = curi + 1;
+                            curj = curj + 1;
+                            int newDessert = dessertMap[curi][curj];
+                            if (!visited[newDessert]) {
+                                //cout << "[downright] (" << curi << ", " << curj << ")" << endl;
+                                visited[newDessert] = true;
+                            }
+                            else {
+                                isAble = false;
+                                break;
+                            }
+                        }
+
+                        if (!isAble)
+                            continue;
+
+                        for (int n = 0; n < b; n++) {
+                            curi = curi + 1;
+                            curj = curj - 1;
+                            int newDessert = dessertMap[curi][curj];
+                            if (!visited[newDessert]) {
+                                //cout << "[downleft] (" << curi << ", " << curj << ")" << endl;
+                                visited[newDessert] = true;
+                            }
+                            else {
+                                isAble = false;
+                                break;
+                            }
+                        }
+
+                        if (!isAble)
+                            continue;
+
+                        for (int n = 0; n < a; n++) {
+                            curi = curi - 1;
+                            curj = curj - 1;
+                            int newDessert = dessertMap[curi][curj];
+                            if (!visited[newDessert]) {
+                                //cout << "[upleft] (" << curi << ", " << curj << ")" << endl;
+                                visited[newDessert] = true;
+                            }
+                            else {
+                                isAble = false;
+                                break;
+                            }
+                        }
+
+                        if (!isAble)
+                            continue;
+
+                        for (int n = 0; n < b - 1; n++) {
+                            curi = curi - 1;
+                            curj = curj + 1;
+                            int newDessert = dessertMap[curi][curj];
+                            if (!visited[newDessert]) {
+                                //cout << "[upright] (" << curi << ", " << curj << ")" << endl;
+                                visited[newDessert] = true;
+                            }
+                            else {
+                                isAble = false;
+                                break;
+                            }
+                        }
+
+                        if (!isAble)
+                            continue;
+
+                        result = (a + b) * 2;
+                    }
+                }
             }
         }
     }
 
-    for (int i = 0; i < a.size(); i++) {
-        int x = a[i].x;
-        int y = a[i].y;
-        int count = a[i].count;
-        while (x + 1 >= 0 && x + 1 < m && y - 1 >= 1 && y - 1 < m && a[i].visited[dessertMap[x + 1][y - 1]] == false) {
-            x = x + 1;
-            y = y - 1;
-            count++;
-            a[i].visited[dessertMap[x][y]] = true;
-            Point tempPoint(x, y, a[i].startX, a[i].startY, count, a[i].visited);
-            b.push_back(tempPoint);
-        }
-    }
-
-    for (int i = 0; i < b.size(); i++) {
-        int x = b[i].x;
-        int y = b[i].y;
-        int count = b[i].count;
-        while (x - 1 >= 1 && x - 1 < m && y - 1 >= 0 && y - 1 < m && b[i].visited[dessertMap[x - 1][y - 1]] == false) {
-            x = x - 1;
-            y = y - 1;
-            count++;
-            b[i].visited[dessertMap[x][y]] = true;
-            Point tempPoint(x, y, b[i].startX, b[i].startY, count, b[i].visited);
-            c.push_back(tempPoint);
-        }
-    }
-
-    int maxCount = -1;
-    for (int i = 0; i < c.size(); i++) {
-        int x = c[i].x;
-        int y = c[i].y;
-        int count = c[i].count;
-        while (x - 1 >= 0 && x - 1 < m && y + 1 >= 0 && y + 1 < m && c[i].visited[dessertMap[x - 1][y + 1]] == false) {
-            x = x - 1;
-            y = y + 1;
-            count++;
-            c[i].visited[dessertMap[x][y]] = true;
-            Point tempPoint(x, y, c[i].startX, c[i].startY, count, c[i].visited);
-            d.push_back(tempPoint);
-        }
-        if (x - 1 == c[i].startX && y + 1 == c[i].startY) {
-            maxCount = max(maxCount, count);
-        }
-    }
-
-    return maxCount;
+    return result;
 }
 
 int main() {
     int n;
     cin >> n;
+
+    vector<int> res(n);
     for (int i = 0; i < n; i++) {
         int m;
         cin >> m;
@@ -111,6 +113,10 @@ int main() {
             }
         }
 
-        cout << "#" << i + 1 << " " << solve(dessertMap, m) << endl;
+        res[i] = solve(dessertMap, m);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "#" << i + 1 << " " << res[i] << endl;
     }
 }
